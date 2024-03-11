1efabf56402d8b8e3c9b507f42c11d64f3d31e25
Vitor Hugo <vitor_hugo_di@hotmail.com>
2024-03-11 19:47:28 -0300

    chore: cria config aws

---
+++ config/Credentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+    AwsConfig: {
+        "region": "sa-east-2",
+        "endpoint": "http://dynamodb.sa-east-1.amazonaws.com",
+        "accessKeyId": "SECRET_KEY", "secretAccessKey": "SECRET_ACCESS_KEY"
+    }
+}
